a8afff07f363500389a74c24fffc859c4c1ce69b
Oskar Barcz <oskar@codingisthinking.com>
2020-02-14 21:24:28 +0100

    Reconfigure Docker to separate its networks

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\ResourceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
+    private ResourceRepository $resourceRepository;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager, ResourceRepository $resourceRepository)
+    {
+        $this->resourceRepository = $resourceRepository;
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/")
      */
     public function index(): Response
     {
-        return $this->render('admin/admin.html.twig');
+        return $this->render('admin/admin.html.twig', ['resources' => $this->resourceRepository->findAll()]);
     }
 }
