bdf7c61ffd35c440157b730fee763e25d69d3061
Oskar Barcz <oskar@codingisthinking.com>
2020-02-14 20:02:15 +0100

    Install webpack Encore

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class            => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                    => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class               => ['all' => true],
 ];
