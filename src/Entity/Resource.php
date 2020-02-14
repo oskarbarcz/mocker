610003a476e1ec1715b218ebbdab2ae42bb28e79
Oskar Barcz <oskar@codingisthinking.com>
2020-02-14 20:53:03 +0100

    Add entity model

---
+++ src/Entity/Resource.php
@@ -0,0 +1,91 @@
+<?php declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ResourceRepository")
+ */
+class Resource
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $uuid;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $slug;
+
+    /**
+     * @ORM\Column(type="json", nullable=true)
+     */
+    private ?array $content = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): ?string
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(string $uuid): self
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?array
+    {
+        return $this->content;
+    }
+
+    public function setContent(?array $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
---
+++ src/Repository/ResourceRepository.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Resource;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method Resource|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Resource|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Resource[]    findAll()
+ * @method Resource[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ResourceRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Resource::class);
+    }
+}
