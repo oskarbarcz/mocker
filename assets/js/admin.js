277836e327c5383f06992218fb5f23651f09f54e
Oskar Barcz <oskar@codingisthinking.com>
2020-02-16 12:50:45 +0100

    Add syntax highligher

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,1 +1,7 @@
+import hljs from 'highlight.js/lib/highlight';
+import json from 'highlight.js/lib/languages/json';
+import 'highlight.js/styles/atom-one-dark.css';
 import '../sass/domain/admin/index.sass';
+
+hljs.registerLanguage('javascript', json);
+hljs.initHighlightingOnLoad();
