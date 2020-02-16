872da78f404fc12dfcdc64358d5cef6054c72733
Oskar Barcz <oskar@codingisthinking.com>
2020-02-16 13:19:23 +0100

    Add JSON prettyfier

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,7 +1,17 @@
 import hljs from 'highlight.js/lib/highlight';
 import json from 'highlight.js/lib/languages/json';
-import 'highlight.js/styles/atom-one-dark.css';
+import 'highlight.js/styles/atom-one-light.css';
 import '../sass/domain/admin/index.sass';
 
-hljs.registerLanguage('javascript', json);
-hljs.initHighlightingOnLoad();
+if (document.querySelector('.details').getAttribute('data-hasCode')) {
+  // make JSON pretty with internal tools
+  const code = document.querySelector('code.json');
+  const parsed = JSON.parse(code.innerText);
+  code.innerText = JSON.stringify(parsed, null, 2);
+
+  // init highlighter
+  hljs.registerLanguage('javascript', json);
+  hljs.initHighlightingOnLoad();
+
+}
+
