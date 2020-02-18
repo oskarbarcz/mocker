e05664464d6a3a7ff2f74221309f239b85239ecc
Oskar Barcz <oskar@codingisthinking.com>
2020-02-18 17:19:19 +0100

    Add PHP translation keys

--- src/Form/ResourceType.php
+++ src/Form/ResourceType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Resource;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +15,42 @@ class ResourceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('slug')
-            ->add('content');
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'label' => 'forms.fields.label.name',
+                    'help'  => 'forms.fields.help.name',
+                ]
+            )
+            ->add(
+                'slug',
+                TextareaType::class,
+                [
+                    'required' => false,
+                    'label'    => 'forms.fields.label.slug',
+                    'help'     => 'forms.fields.help.slug',
+                ]
+            )
+            ->add(
+                'description',
+                TextareaType::class,
+                [
+                    'required' => false,
+                    'label'    => 'forms.fields.label.description',
+                    'help'     => 'forms.fields.help.description',
+
+                ]
+            )
+            ->add(
+                'content',
+                TextareaType::class,
+                [
+                    'required' => false,
+                    'label'    => 'forms.fields.label.content',
+                    'help'     => 'forms.fields.help.content',
+                ]
+            );
     }
 
     /** @inheritDoc */
@@ -23,7 +58,8 @@ class ResourceType extends AbstractType
     {
         $resolver->setDefaults(
             [
-                'data_class' => Resource::class,
+                'data_class'         => Resource::class,
+                'translation_domain' => 'forms',
             ]
         );
     }
